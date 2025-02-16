2a5a219834d41eb9ed2b4da6e76162295072e5d4
Haruka <haruka@dy.ax>
2025-02-16 17:14:11 +0800

    重构项目结构，删除不必要的文件，添加全局样式和提供者组件

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,144 @@
-import Image from "next/image";
+"use client";
+
+import { Link } from "@heroui/react";
+import { motion } from "framer-motion";
+import { lazy, useEffect, useState } from "react";
+
+const ease = [0.16, 1, 0.3, 1];
+const LazySpline = lazy(() => import("@splinetool/react-spline"));
+
+interface Host {
+  api: string;
+  site: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [config, setConfig] = useState({
+    siteName: "",
+    telegramGroup: "",
+    telegramContact: "",
+    domains: [],
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const checkDomain = (domains: Array<Host | string>) => {
+    for (let i = 0; i < domains.length; i++) {
+      let api: string;
+      let site: string;
+
+      const host = domains[i];
+      if (typeof host === "string") {
+        api = host;
+        site = host;
+      } else {
+        api = host.api;
+        site = host.site;
+      }
+      fetch(api).then((res) => {
+        if (res.ok && res.status === 200) {
+          window.location.href = site + location.search;
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetch("/config.json").then(async (res) => {
+      const data = await res.json();
+      setConfig(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (config.domains.length === 0) return;
+
+    const timeOut = setTimeout(() => {
+      checkDomain(config.domains);
+      const timer = setInterval(() => {
+        checkDomain(config.domains);
+      }, 3000);
+      setTimeout(() => {
+        clearInterval(timer);
+        const text =
+          "若卡在此页面无法跳转可用站点，请点击右下角信息图标联系客服";
+        alert(text);
+        document.getElementById("info")!.innerText = text;
+      }, 60000);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [config]);
+
+  return (
+    <main className="bg-[#2b2b2b] h-screen w-full flex flex-row justify-center items-center">
+      <div className="flex flex-col-reverse w-full lg:grid lg:grid-cols-2 p-6 lg:p-12 overflow-hidden">
+        <div className="flex h-full w-full max-w-3xl flex-col overflow-hidden pt-8 justify-center items-center gap-4">
+          <motion.h1
+            className="text-left text-4xl text-center font-semibold leading-tighter text-foreground sm:text-5xl md:text-6xl"
+            initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+            animate={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+            transition={{
+              duration: 1,
+              ease,
+              staggerChildren: 0.2,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <motion.span
+              className="text-balance text-white"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease,
+              }}
+            >
+              {config.siteName} <br />
+              线路检测页
+            </motion.span>
+          </motion.h1>
+          <motion.p
+            className="text-center text-lg text-balance"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.6,
+              duration: 0.8,
+              ease,
+            }}
+            id="info"
           >
-            Read our docs
-          </a>
+            线路测试中
+            <br />
+            将自动跳转最快域名
+            <br />
+            请耐心等候
+          </motion.p>
+          <motion.span
+            className="inline-block text-sm text-white"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.0, duration: 0.8 }}
+          >
+            Telegram 群组{" "}
+            <Link href={config.telegramGroup}>
+              @{config.telegramGroup.split("https://t.me/")[1]}
+            </Link>
+          </motion.span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 1 }}
+          className="relative h-96"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <LazySpline
+            scene="/scene.splinecode"
+            className="absolute w-full h-full flex items-center justify-center"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </motion.div>
+      </div>
+    </main>
   );
 }
